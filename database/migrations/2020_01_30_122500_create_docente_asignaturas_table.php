73f7d1a97699d80471941155946cdb5e20f07265
Joel Rivera <31928575+Joel97c@users.noreply.github.com>
2020-02-01 19:21:51 -0500

    010220

--- database/migrations/2020_01_30_122500_create_docente_asignaturas_table.php
+++ database/migrations/2020_01_30_122500_create_docente_asignaturas_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateDocenteAsginaturasTable extends Migration
+class CreateDocenteAsignaturasTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateDocenteAsginaturasTable extends Migration
      */
     public function up()
     {
-        Schema::create('docente_asginaturas', function (Blueprint $table) {
+        Schema::create('docente_asignaturas', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->timestamps();
                     $table->integer('asignatura_id');
@@ -36,6 +36,6 @@ class CreateDocenteAsginaturasTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('docente_asginaturas');
+        Schema::dropIfExists('docente_asignaturas');
     }
 }
